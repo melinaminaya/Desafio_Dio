c4fcdb857bc6a422dcb81aa052ee8fd8061012d7
Melina Minaya <melina.minaya@autotrac.com.br>
2024-10-21 11:01:20 -0300

    Insere novo desafio POO
    - Contém alguns erros de @log property a serem saldados.

--- SistemaBancarioPy/desafio.py
+++ SistemaBancarioPy/desafio.py
@@ -61,3 +61,56 @@ while True:
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+# def main():
+#     def analise_vendas(vendas):
+#         total_vendas = sum(vendas)
+#         media_vendas = total_vendas / len(vendas)
+    
+    
+#         return f"{total_vendas}, {media_vendas:.2f}"
+
+#     def obter_entrada_vendas():
+#         # Solicita a entrada do usuário em uma única linha
+#         entrada = input()
+#         numeros_como_strings = entrada.split(',')
+#         vendas = list(map(int, numeros_como_strings))
+    
+#         return vendas
+
+#     vendas = obter_entrada_vendas()
+#     print(analise_vendas(vendas))
+
+# main()
+
+# def produto_mais_vendido(produtos):
+#     contagem = {}
+    
+#     for produto in produtos:
+#         if produto in contagem:
+#             contagem[produto] += 1
+#         else:
+#             contagem[produto] = 1
+    
+#     max_produto = None
+#     max_count = 0
+    
+#     for produto, count in contagem.items():
+#         # TODO: Encontre o produto com a maior contagem:
+#         if(count > max_count):
+#           max_count = count
+#           max_produto = produto
+    
+#     return max_produto
+
+# def obter_entrada_produtos():
+#     # Solicita a entrada do usuário em uma única linha
+#     entrada = input()
+#     # TODO: Converta a entrada em uma lista de strings, removendo espaços extras:
+#     lista_produtos = [produto.strip() for produto in entrada.split(",")]
+#     produtos = lista_produtos
+    
+#     return produtos
+
+# produtos = obter_entrada_produtos()
+# print(produto_mais_vendido(produtos))
